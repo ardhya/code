ba21116dca35b16f1e1c82cef0b2785c19e0fe43
ardhya <iwn.sulistyo@gmail.com>
2016-05-23 00:02:45 +0700

    Update cara-pembelian.js

--- cara-pembelian.js
+++ cara-pembelian.js
@@ -1,4 +1,3 @@
-<div class="module">
 <script type="text/javascript">
 document.write("<img src='https://4.bp.blogspot.com/-IEE-NHlF1PQ/Vz_okh3xslI/AAAAAAAABxc/jdg-Ylp3NzQc5o3m9AUbvrP8FRVYwQoRQCLcB/s1600/cara-beli-obat-herbal-online.jpg'>");
 </script>
